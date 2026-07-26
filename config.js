9229924d820b1be352ddaf1ca52d4fb48b57cbd7
GitaGovindaDasi108 <rkbrewer@gmail.com>
2026-07-26 14:52:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 window.TBS_CONFIG = {
 
   // Apps Script web-app address, ending in /exec.
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyaoCEz4_4mVl3Gl-JivPYFgZO2avIxoZq_c2DsM5UHYEXIx22X5gcmTn3sHkICV3n5/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx4Cm8WoHhMiDjS1u7T307A6tdK0nwNvesLzUe343DrsUWfADdoK3-NKSFR8wyvvNX8/exec',
 
   // Header photographs. Leave as-is unless you rename the files.
   PRABHUPADA_IMG: 'Srila_Prabhupada.jpeg',
